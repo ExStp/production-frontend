d2d2211d47ef3c67aa478a0ac7bf70fceda8700f
Andrey <araibov@mail.ru>
2023-11-08 10:53:51 +0500

    refactor webpack config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,14 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+export const buildPlugins = (options: BuildOptions): webpack.WebpackPluginInstance[] => {
+	const { mode, paths } = options;
+	
+	return [
+		new HtmlWebpackPlugin({
+			template: paths.html,
+		}),
+		new webpack.ProgressPlugin(),
+	];
+};
