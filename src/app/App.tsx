6f554ce1c08b4da39eba39dd1973362dde597a7a
Andrey <araibov@mail.ru>
2024-01-23 16:12:21 +0500

    13 lesson finish

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,20 @@
-import "./styles/index.scss";
-import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "app/providers/ThemeProvider";
-import { AppRouter } from "app/providers/router";
-import { Navbar } from "widgets/Navbar";
+import React from 'react';
+import './styles/index.scss';
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+
 
 const App = () => {
-	const { theme } = useTheme();
+   const { theme } = useTheme();
 
-	return (
-		<div className={classNames("app", {}, [theme])}>
-			<Navbar />
-			<AppRouter />
-		</div>
-	);
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <AppRouter />
+        </div>
+    );
 };
 
 export default App;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,29 +1,25 @@
-import { classNames } from "shared/lib/classNames/classNames";
-import cls from "./Navbar.module.scss";
-import { RoutePath } from "shared/config/routeConfig/routeConfig";
-import { AppLink, AppLinkTheme } from "shared/ui/AppLink";
-import { ThemeSwitcher } from "widgets/ThemeSwitcher/ui/ThemeSwitcher";
+import React from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Navbar.module.scss';
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
-	className?: string;
+    className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-	return (
-		<div className={classNames(cls.navbar, {}, [className])}>
-			<ThemeSwitcher />
-			<div className={cls.links}>
-				<AppLink
-					theme={AppLinkTheme.SECONDARY}
-					to={RoutePath.main}
-					className={cls.mainLink}
-				>
-					Главная
-				</AppLink>
-				<AppLink theme={AppLinkTheme.RED} to={RoutePath.about}>
-					О сайте
-				</AppLink>
-			</div>
-		</div>
-	);
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames (cls.navbar, {}, [className])}>
+            <ThemeSwitcher />
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
+                    Главная
+                </AppLink>
+                <AppLink theme={AppLinkTheme.RED} to={'/about'}>
+                    О сайте
+                </AppLink>
+            </div>
+        </div>
+    );
 };
